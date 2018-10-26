4b1f5d8ff2b480478e3d93d18a91111502ed2cff
kaznishi <kaz.nishimu@gmail.com>
2018-10-26 21:18:23 +0900

    .

--- controller/article_controller.go
+++ controller/article_controller.go
@@ -2,10 +2,12 @@ package controller
 
 import (
 	"fmt"
+	"github.com/kaznishi/blog_tutorial_golang/model/data_model"
+	"html/template"
 	"net/http"
 
 	"github.com/kaznishi/blog_tutorial_golang/service"
-	)
+)
 
 type ArticleController struct {
 	ArticleService service.ArticleService
@@ -23,13 +25,23 @@ func (c *ArticleController) Index(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprint(w, "一覧ページ,エラー")
 		fmt.Fprint(w, err)
 	}
-	fmt.Fprint(w, "一覧ページ\n")
-	for _, article := range articles {
-		fmt.Fprint(w, article.ID)
-		fmt.Fprint(w, ", ")
-		fmt.Fprint(w, article.Title)
-		fmt.Fprint(w, "\n")
-		fmt.Fprint(w, article.Content)
-		fmt.Fprint(w, "\n======\n")
+
+	tmpl, err := template.ParseFiles("view/layout.html.tmpl", "view/article/index.html.tmpl")
+	if err != nil {
+		fmt.Fprint(w, "一覧ページ,エラー")
+		fmt.Fprint(w, err)
+	}
+
+	data := struct {
+		Title string
+		Articles []*data_model.Article
+	}{
+		Title: "一覧ページ",
+		Articles: articles,
+	}
+
+	if err := tmpl.Execute(w, data); err != nil {
+		fmt.Fprint(w, "一覧ページ,エラー")
+		fmt.Fprint(w, err)
 	}
 }
