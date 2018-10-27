3d15c2728a8321e79ca09f7188d08a3cbef6d220
kaznishi <kaz.nishimu@gmail.com>
2018-10-27 15:55:32 +0900

    .

--- controller/admin_controller.go
+++ controller/admin_controller.go
@@ -2,9 +2,11 @@ package controller
 
 import (
 	"fmt"
+	"github.com/gorilla/mux"
 	"github.com/kaznishi/blog_tutorial_golang/model/data_model"
 	"github.com/kaznishi/blog_tutorial_golang/service"
 	"net/http"
+	"strconv"
 )
 
 type AdminController struct {
@@ -80,6 +82,58 @@ func (c *AdminController) NewArticle(w http.ResponseWriter, r *http.Request) {
 
 	if err := tmpl.ExecuteTemplate(w, "layout_admin.html.tmpl", data); err != nil {
 		fmt.Fprint(w, "新規記事投稿フォーム,エラー")
+		fmt.Fprint(w, err)
+	}
+}
+
+func (c *AdminController) EditArticle(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id, err := strconv.Atoi(vars["id"])
+	if err != nil {
+		fmt.Errorf("Error : %s", err)
+		fmt.Fprint(w, "Error : ", err)
+		return
+	}
+	a, err := c.ArticleService.GetById(id)
+	if err != nil {
+		fmt.Errorf("Error : %s", err)
+		fmt.Fprint(w, "Error : ", err)
+		return
+	}
+
+	//if r.Method == http.MethodPost {
+	//	if err := r.ParseForm(); err != nil {
+	//		fmt.Fprint(w, "記事編集フォーム,エラー")
+	//		fmt.Fprint(w, err)
+	//	}
+	//	a.Title = r.FormValue("title")
+	//	a.Content = r.FormValue("content")
+	//
+	//	_, err := c.ArticleService.CreateArticle(a)
+	//	if err != nil {
+	//		fmt.Fprint(w, "記事編集フォーム,エラー")
+	//		fmt.Fprint(w, err)
+	//	} else {
+	//		http.Redirect(w, r, "/admin/", 301)
+	//	}
+	//}
+
+	tmpl, err := initializeTemplate().ParseFiles("view/layout_admin.html.tmpl", "view/admin/edit_article.html.tmpl")
+	if err != nil {
+		fmt.Fprint(w, "記事編集フォーム,エラー")
+		fmt.Fprint(w, err)
+	}
+
+	data := struct {
+		Title string
+		Article *data_model.Article
+	}{
+		Title: "記事編集フォーム",
+		Article: a,
+	}
+
+	if err := tmpl.ExecuteTemplate(w, "layout_admin.html.tmpl", data); err != nil {
+		fmt.Fprint(w, "記事編集フォーム,エラー")
 		fmt.Fprint(w, err)
 	}
 
